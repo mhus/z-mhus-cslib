f8c18c74de8b1f549c348d20c257e5a39f14cb68
Mike Hummel <>
2019-05-14 16:24:42 +0200

    blowfish tooling

--- mhus-cslib/MBouncy.cs
+++ mhus-cslib/MBouncy.cs
@@ -6,11 +6,51 @@ using Org.BouncyCastle.Crypto.Encodings;
 using Org.BouncyCastle.Crypto.Engines;
 using Org.BouncyCastle.Crypto.Parameters;
 using Org.BouncyCastle.OpenSsl;
+using Org.BouncyCastle.Crypto.Paddings;
 
 namespace mhuscslib
 {
     public class MBouncy
     {
+
+        static readonly Encoding Encoding = Encoding.UTF8;
+
+        public static string EncryptBlowfish(string secret, string key)
+        {
+            BlowfishEngine engine = new BlowfishEngine();
+
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(engine);
+
+            KeyParameter keyBytes = new KeyParameter(Encoding.GetBytes(key));
+
+            cipher.Init(true, keyBytes);
+
+            byte[] inB = Encoding.GetBytes(secret);
+            byte[] outB = new byte[cipher.GetOutputSize(inB.Length)];
+            int len1 = cipher.ProcessBytes(inB, 0, inB.Length, outB, 0);
+
+            cipher.DoFinal(outB, len1);
+
+            return Convert.ToBase64String(outB);
+        }
+
+        public static string DecryptBlowfish(string encrypted, string key)
+        {
+            BlowfishEngine engine = new BlowfishEngine();
+            PaddedBufferedBlockCipher cipher = new PaddedBufferedBlockCipher(engine);
+
+            cipher.Init(false, new KeyParameter(Encoding.GetBytes(key)));
+
+            byte[] out1 = Convert.FromBase64String(encrypted);
+            byte[] out2 = new byte[cipher.GetOutputSize(out1.Length)];
+            int len2 = cipher.ProcessBytes(out1, 0, out1.Length, out2, 0);
+
+            cipher.DoFinal(out2, len2);
+
+            return Encoding.GetString(out2);
+
+        }
+
         public static string EncryptRsa(string clearText, string publicKey)
         {
             var bytesToEncrypt = Encoding.UTF8.GetBytes(clearText);
--- mhus-cslib/Program.cs
+++ mhus-cslib/Program.cs
@@ -7,6 +7,21 @@ namespace mhuscslib
         public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            string key = "hellohellohello";
+            string text = "Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
+            	"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum " +
+            	"Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum Lorem Ipsum.";
+            string encrypted = MBouncy.EncryptBlowfish(text, key);
+
+            Console.WriteLine("Encrypted");
+            Console.WriteLine(encrypted);
+
+            string decrypted = MBouncy.DecryptBlowfish(encrypted, key);
+
+            Console.WriteLine("Decrypted");
+            Console.WriteLine(decrypted);
+
         }
     }
 }
